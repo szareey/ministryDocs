c4ad61d07db6a262e4b58dd783369c50fd1944d4
Sergey Popov <quolpr@gmail.com>
2015-08-07 00:50:03 +0300

    Added openstruct to doc

--- lib/ministry_docs/math_parser.rb
+++ lib/ministry_docs/math_parser.rb
@@ -7,10 +7,11 @@ module MinistryDocs
       file = page.search(selector)
                  .attribute('href').value
       courses = parse(open(URL + file).read.encode('UTF-8', 'Windows-1251'))
-      {
-        courses: courses,
-        subject: 'math'
-      }.merge(addable_info(page))
+      doc = {
+              courses: courses,
+              subject: 'math'
+            }.merge(addable_info(page))
+      OpenStruct.new(doc)
     end
 
     protected
--- spec/ministry_docs/math_2005_parser_spec.rb
+++ spec/ministry_docs/math_2005_parser_spec.rb
@@ -4,6 +4,6 @@ describe MinistryDocs::Math2005Parser, :vcr do
   subject(:math_parser){ MinistryDocs::Math2005Parser.new }
 
   it 'parse file without errors' do
-    expect{p math_parser.parse_from_site}.not_to raise_error
+    expect{math_parser.parse_from_site}.not_to raise_error
   end
 end
